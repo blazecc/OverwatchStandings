2a3b53e328cf61f955be3e1b742d5816e6288ed6
Blazecc <mercyme19892003@gmail.com>
2018-02-10 11:05:59 -0500

    deleting unnecessary files, adding .gitignore for .vs directory, and adding mock data for Teams

---
+++ OverwatchStandings/Data/Teams.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OverwatchStandings.Models;
+
+namespace OverwatchStandings.Data
+{
+    public class Teams
+    {
+        public List<Team> OWLTeams = new List<Team>
+        {
+            new Team
+            {
+                Id = 1,
+                Name = "Boston Uprising"
+            },
+            new Team
+            {
+                Id = 2,
+                Name = "Dalas Fuel"
+            }, new Team
+            {
+                Id = 3,
+                Name = "Florid aMayhem"
+            }, new Team
+            {
+                Id = 4,
+                Name = "HoustonOutlaws"
+            }, new Team
+            {
+                Id = 5,
+                Name = "LondonSpitfire"
+            }, new Team
+            {
+                Id = 6,
+                Name = "Los Angeles Gladiators"
+            }, new Team
+            {
+                Id = 7,
+                Name = "Los Angeles Valiant"
+            }, new Team
+            {
+                Id = 8,
+                Name = "New York Excelsior"
+            }, new Team
+            {
+                Id = 9,
+                Name = "Philadelphia Fusion"
+            }, new Team
+            {
+                Id = 10,
+                Name = "San Francisco Shock"
+            }, new Team
+            {
+                Id = 11,
+                Name = "Seoul Dynasty"
+            }, new Team
+            {
+                Id = 12,
+                Name = "Shanghai Dragons"
+            }
+        };
+    }
+}
